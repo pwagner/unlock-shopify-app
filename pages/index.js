f67d675967c0b04374da7402dec3dc8ff7648e0d
Paul Wagner <npm.pwagner@icloud.com>
2021-04-18 11:56:10 +0200

    started listing locks in admin

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import { Heading, Page } from "@shopify/polaris";
 
 const Index = () => (
   <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
+    <Heading>Locks</Heading>
+    <p>TODO: List with enable/disable control</p>
   </Page>
 );
 
